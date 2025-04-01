a84859dfae9b3c323531ab534c15347655c38361
fbergami57 <fabiobergami57@gmail.com>
2025-03-31 22:59:41 -0300

    BergamiDell Upadte

--- C#/String/String/Program.cs
+++ C#/String/String/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+var id = Guid.NewGuid();
+id.ToString();
+
+id = new Guid("c9eb1672-d40b-4d7d-92aa-b0824c9dabd6");
+
+Console.WriteLine(id.ToString().Substring(0, 8));
