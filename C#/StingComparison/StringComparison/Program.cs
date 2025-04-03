afe19366488ceeb6e560bb4b02fc9b7a458c0369
fbergami57 <fabiobergami57@gmail.com>
2025-04-02 21:12:26 -0300

    BergamiDell update

---
+++ C#/StingComparison/StringComparison/Program.cs
@@ -0,0 +1,9 @@
+﻿var text = "Testing";
+Console.WriteLine(text.CompareTo("Testing"));
+Console.WriteLine(text.CompareTo("testing"));
+
+var text1 = "This text is a test";
+Console.WriteLine(text1.Contains("test"));
+Console.WriteLine(text1.Contains("Test"));
+Console.WriteLine(text1.Contains("Test", StringComparison.OrdinalIgnoreCase));
+Console.WriteLine(text1.Contains(null));
